d3d9da67573864b99efda07b55f76549b5cd31ff
Alex Cunningham <alexcunn@k9alk.love>
2025-03-31 04:09:06 -0400

    adding codebase

---
+++ packages/test/index.test.ts
@@ -0,0 +1,18 @@
+import { TestClient } from './index';
+import dotenv from 'dotenv';
+dotenv.config();
+
+describe('TestClient', () => {
+    it('should log "[Test Client] listening for messages..." when connected', async () => {
+
+        const testClient = new TestClient();
+
+        const consoleSpy = jest.spyOn(console, 'log');
+
+        await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for 1 second
+
+        expect(consoleSpy).toHaveBeenCalledWith('[Test Client] listening for messages...');
+
+        consoleSpy.mockRestore();
+    });
+});
