2836856faa052dd07c6e0dc9c85c0962c581d715
Alex Cunningham <alexcunn@k9alk.love>
2025-03-31 04:28:46 -0400

    checking if secrets exist

--- packages/test/index.test.ts
+++ packages/test/index.test.ts
@@ -2,6 +2,19 @@ import { TestClient } from './index';
 import dotenv from 'dotenv';
 dotenv.config();
 
+
+if (process.env.BOT_TOKEN) {
+    console.log("BOT_TOKEN is present in bun test");
+} else {
+    console.log("BOT_TOKEN is NOT present in bun test");
+}
+
+if (process.env.CLIENT_ID) {
+    console.log("CLIENT_ID is present in bun test");
+} else {
+    console.log("CLIENT_ID is NOT present in bun test");
+}
+
 describe('TestClient', () => {
     it('should log "[Test Client] listening for messages..." when connected', async () => {
 
